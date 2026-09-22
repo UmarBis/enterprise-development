3088c7616ffc9f94f3943afba477665303d07712
UmarBis <bis.umar@icloud.com>
2026-09-22 21:59:29 +0400

    Update Client.cs

--- src/FitnessClub.Domain/Entities/Client.cs
+++ src/FitnessClub.Domain/Entities/Client.cs
@@ -1,47 +1,23 @@
 namespace FitnessClub.Domain.Entities;
 
 /// <summary>
-/// Клиент финтес-клуба.
+/// Клиент фитнес-клуба.
 /// </summary>
-public class Client
+public class Client : Person
 {
-    /// <summary>Идентификатор клиента.</summary>
-    public Guid Id { get; init; } = Guid.NewGuid();
-
-    /// <summary>Имя.</summary>
-    public required string FirstName { get; init; }
-
-    /// <summary>Фамилия.</summary>
-    public required string LastName { get; init; }
-
     /// <summary>Контактный телефон.</summary>
-    /// <example>+7-900-100-10-01</example>
     public required string Phone { get; init; }
 
-    /// <summary>Электронная почта.</summary>
-    public required string Email { get; init; }
+    /// <summary>Дата начала абонемента.</summary>
+    public DateOnly MembershipStartDate { get; init; }
 
-    /// <summary>Дата рождения.</summary>
-    public DateOnly BirthDate { get; init; }
+    /// <summary>Дата окончания абонемента.</summary>
+    public DateOnly MembershipEndDate { get; init; }
 
-    /// <summary>Дата регистрации в клубе.</summary>
-    public DateOnly RegistrationDate { get; init; }
+    /// <summary>Проверяет, активен ли абонемент на указанную дату.</summary>
+    public bool IsMembershipActive(DateOnly onDate)
+        => onDate >= MembershipStartDate && onDate <= MembershipEndDate;
 
-    /// <summary>Полное имя клиента.</summary>
-    public string FullName => $"{FirstName} {LastName}";
-
-    /// <summary>
-    /// Возвращает количество полных лет на указанную дату.
-    /// </summary>
-    /// <param name="onDate">Дата, на которую вычисляется возраст.</param>
-    public int GetAge(DateOnly onDate)
-    {
-        var age = onDate.Year - BirthDate.Year;
-        if (onDate < BirthDate.AddYears(age))
-        {
-            age--;
-        }
-
-        return age;
-    }
+    /// <summary>Проверяет, просрочен ли абонемент на указанную дату.</summary>
+    public bool IsMembershipExpired(DateOnly onDate) => onDate > MembershipEndDate;
 }
