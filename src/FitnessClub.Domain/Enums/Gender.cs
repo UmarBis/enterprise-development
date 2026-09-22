594ef829c98c3b8d660956f3a8008a19ab9cc12f
UmarBis <bis.umar@icloud.com>
2026-09-22 21:56:29 +0400

    Create Gender.cs

---
+++ src/FitnessClub.Domain/Enums/Gender.cs
@@ -0,0 +1,13 @@
+namespace FitnessClub.Domain.Enums;
+
+/// <summary>
+/// Пол персоны.
+/// </summary>
+public enum Gender
+{
+    /// <summary>Мужской.</summary>
+    Male = 1,
+
+    /// <summary>Женский.</summary>
+    Female = 2,
+}
